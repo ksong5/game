d588a50a5fca8277bfba39af0dc4f6f89ab147b3
ksong5 <k.song5@share.epsb.ca>
2022-11-01 11:23:10 -0600

    e

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ let cnv = document.getElementById("canvas");
 let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 800;
+ctx.translate(400, 400);
 
 //mouse coordinates
 let mouseX, mouseY;
 
 // mousehandlers
 document.addEventListener("mousemove", mousemoveHandler);
+// document.addEventListener("mousemove", angleCalc);
 document.addEventListener("mousedown", click);
       
 function mousemoveHandler(event) {
@@ -45,31 +47,45 @@ function draw() {
 function drawMenu() {
     // background
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, cnv.width, cnv.height);
+    ctx.fillRect(-400, -400, cnv.width, cnv.height);
 
     // button1
     ctx.fillStyle = "grey";
-    ctx.fillRect(400 - 175 / 2, 600, 175, 60);
+    ctx.fillRect(-75, -30, 150, 60);
 
     // button1 text
     ctx.font = "40px Arial"
     ctx.fillStyle = "black";
-    ctx.fillText("START", 335, 645);
+    ctx.fillText("START", -65, 15);
 }
 
 // start button function
 function click() {
     console.log(mouseX, mouseY);
 
-    if (gamestate === "menu" && mouseX > 314 && mouseX < 314 + 175 && mouseY > 600 && mouseY < 600 + 60) {
+    if (gamestate === "menu" && mouseX > 326 && mouseX < 475 && mouseY > 371 && mouseY < 429) {
         gamestate = "gaming";
         console.log(gamestate);
     }
 }
 
 function drawGame() {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, cnv.width, cnv.height);
+    ctx.rotate(10 * Math.PI / 180);
 
-    ctx.fillStyle = "";
+    ctx.fillStyle = "white";
+    ctx.fillRect(-200, -200, cnv.width, cnv.height);
+
+    ctx.fillStyle = "grey";
+    ctx.arc(0, 0, 35, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.fillStyle = "grey";
+    ctx.fillRect(-10, 0, 20, 70);
 }
+
+// setInterval(angleCalc, 100);
+
+// function angleCalc(mouseX, mouseY) {
+//     Math.atan2(mouseX, mouseY) * 180 / Math.PI;
+// }
+
